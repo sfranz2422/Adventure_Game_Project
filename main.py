a0657f76107b39a7990c848cd118e9cbf5d881a2
Steve <stephenfranz22@gmail.com>
2022-10-31 18:43:59 +0000

    franzversion

--- main.py
+++ main.py
@@ -1,24 +1,93 @@
 from adventurelib import *
 
-
-# > go north
-# I don't understand 'go north'.
-
-# > help
-# Here is a list of the commands you can give:
-# ?
-# help
-# quit
+########################################
+# DEFINE YOUR ROOMS
+house = Room("""
+This is your house.  You know what it looks like. 
 
 
+""")
 
-# The @when decorator is written on the line above a function. The function will then be called when a player types a matching command.
+outside = Room("""
 
-# This code will be called when the player types “scream”:
+It is a pleasant November day. A little cool but sunny. 
 
+""")
+
+
+########################################
+# OPENING TEXT
+print("""
+
+You get out of bed at 6:00am.  You need to get ready for school.
+
+Enter rooms of the house to visit such as "kitchen", "family room" etc or type "leave house" to go to school.
+
+You can view your inventory by typing 'bag'.
+
+You can view your current surroundings by typing 'look around'
+
+
+""")
+
+#####################################
+
+# DECLARE YOUR CURRENT ROOM AND CONSTANTS
+current_room = house
+inventory = []
+
+########################################
+
+
+@when('look around')
+def where():
+  print(current_room)
+
+
+def print_house():
+  print(""" 
+         `'::.
+    _________H ,%%&%,
+   /\     _   \%&&%%&%
+  /  \___/^\___\%&%%&&
+  |  | []   [] |%\Y&%'
+  |  |   .-.   | ||  
+~~@._|@@_|||_@@|~||~~~~~~~~~~~~~
+     `""`
+  
+  """)
+
+@when("leave house")
+def outside_of_house():
+  print_house()
+  global current_room
+  current_room = outside
+  print("You walk out the front door of your house.  The road is deserted. \n What do you want to do?  You can 'walk toward school' or 'go back into house'. ")
+
+@when('house')
+@when("go back into house")
+def back_into_house():
+  print("You walk back into the house and find it is exactly how you left it....or is something different? \n You can 'go back outside' or stay in the house")
+  
+
+@when("go back outside")
+def back_outside():
+  print("You walk out the front door again and notice someone sitting in front of you house.  You can 'approach the person' or 'call the police'")
+  
+@when("call the police")
+def call_police():
+  print("You call the police, they are busy and think you are a prank call.")
+
+@when("approach the person")
+def approach_person():
+  print("You approach the person sitting in front of you house.  The person is looking at a comic book.")
+
+
+  
 @when("scream")
 def scream():
-    print("You unleash a piercing shriek that reverberates around you.")
+  print("You unleash a piercing shriek that reverberates around you.")
+  
 
 @when("eat lunch")
 def brush_teeth():
@@ -35,8 +104,6 @@ def brush_teeth():
         run your tongue over them.
     """)
 
-
-
 @when("shout loudly")
 @when("shout")
 @when("yell")
@@ -46,28 +113,24 @@ def yell():
 # take hat
 @when("take THING")
 def take(thing):
+  global inventory
+  inventory.append(thing)
   print(f"You take the {thing}.")
 
+@when("bag")
+def print_inventory():
+  global inventory
+  if len(inventory)==0:
+    print("Your bag is empty")
+  else:
+    for i in inventory:
+      print(i)
+    
 
 @when("give ITEM to RECIPIENT")
 def give(item, recipient):
-  print(f"You give the {item} to the {recipient}.")
-
-
-# try cast fireball
-@when('cast SPELL', context='wonderland')
-def cast(spell):
-  say(f"You cast the {spell} spell.")
-
-@when('enter mirror')
-def enter_mirror():
-    if get_context() == 'wonderland':
-        say('There is no mirror here.')
-    else:
-        set_context('wonderland')
-        say('You step into the silvery surface, which feels wet and cool.')
-        say('You realise that clicking your heels will let you return.')
-
+  if 'chair' in inventory:
+    print(f"You give the {item} to the {recipient}.")
+    inventory.remove(item)
   
-# all code must go above
 start()
